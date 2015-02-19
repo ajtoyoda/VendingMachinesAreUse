042d858d5996f40d7f646d3397fe94ad3930f546
Sean Cox <seanmdcox@gmail.com>
2015-02-18 20:01:21 -0700

    Added change maker test case and updated card manager tests

--- src/iteration2/testCases/TestCardManager.java
+++ src/iteration2/testCases/TestCardManager.java
@@ -34,20 +34,20 @@ public class TestCardManager {
 		final Card card = mockingContext.mock(Card.class);
 		mockingContext.checking(new Expectations() {
 			{
-			one(card).getType();
-			will(returnValue(0));
-			one(card).getNumber();
-			will(returnValue("123456789"));
-			one(card).getName();
-			will(returnValue("John Smith"));
+				one(card).getType();
+				will(returnValue(0));
+				one(card).getNumber();
+				will(returnValue("123456789"));
+				one(card).getName();
+				will(returnValue("John Smith"));
 			}}
 		);
 		final CardSlot cardSlot = mockingContext.mock(CardSlot.class);
 		try {
 			mockingContext.checking(new Expectations() {
 				{
-				one(cardSlot).readCardData();
-				will(returnValue(card));
+					one(cardSlot).readCardData();
+					will(returnValue(card));
 				}}
 			);
 		} catch (EmptyException e) {
@@ -65,20 +65,20 @@ public class TestCardManager {
 		final Card card = mockingContext.mock(Card.class);
 		mockingContext.checking(new Expectations() {
 			{
-			one(card).getType();
-			will(returnValue(0));
-			one(card).getNumber();
-			will(returnValue("123456789"));
-			one(card).getName();
-			will(returnValue("John Smith"));
+				one(card).getType();
+				will(returnValue(0));
+				one(card).getNumber();
+				will(returnValue("123456789"));
+				one(card).getName();
+				will(returnValue("John Smith"));
 			}}
 		);
 		final CardSlot cardSlot = mockingContext.mock(CardSlot.class);
 		try {
 			mockingContext.checking(new Expectations() {
 				{
-				one(cardSlot).readCardData();
-				will(returnValue(card));
+					one(cardSlot).readCardData();
+					will(returnValue(card));
 				}}
 			);
 		} catch (EmptyException e) {
@@ -98,20 +98,20 @@ public class TestCardManager {
 		final Card card = mockingContext.mock(Card.class);
 		mockingContext.checking(new Expectations() {
 			{
-			one(card).getType();
-			will(returnValue(0));
-			one(card).getNumber();
-			will(returnValue("123456789"));
-			one(card).getName();
-			will(returnValue("John Smith"));
+				one(card).getType();
+				will(returnValue(0));
+				one(card).getNumber();
+				will(returnValue("123456789"));
+				one(card).getName();
+				will(returnValue("John Smith"));
 			}}
 		);
 		final CardSlot cardSlot = mockingContext.mock(CardSlot.class);
 		try {
 			mockingContext.checking(new Expectations() {
 				{
-				one(cardSlot).readCardData();
-				will(returnValue(card));
+					one(cardSlot).readCardData();
+					will(returnValue(card));
 				}}
 			);
 		} catch (EmptyException e) {
@@ -123,6 +123,51 @@ public class TestCardManager {
 			assertTrue(cardManager.hasCard());
 			cardManager.cardEjected(cardSlot);
 			assertFalse(cardManager.hasCard());
+	}
+	
+	@Test
+	public void testCharge()
+	{
+
+		Mockery mockingContext = new Mockery();
+		final Card card = mockingContext.mock(Card.class);
+		mockingContext.checking(new Expectations() {
+			{
+				one(card).getType();
+				will(returnValue(0));
+				one(card).getNumber();
+				will(returnValue("123456789"));
+				one(card).getName();
+				will(returnValue("John Smith"));
+				one(card).checkPin("1234");
+				will(returnValue(true));
+				one(card).checkPin("4321");
+				will(returnValue(false));
+				one(card).requestFunds(10, "1234");
+				will(returnValue(0));
+			}}
+		);
+		assertTrue(cardManager.charge(10, "1234"));
+		assertFalse(cardManager.charge(10, "4321"));
+	}
+	
+	@Test
+	public void testVerify()
+	{
+
+		Mockery mockingContext = new Mockery();
+		final Card card = mockingContext.mock(Card.class);
+		mockingContext.checking(new Expectations() {
+			{
+				
+				one(card).checkPin("1234");
+				will(returnValue(true));
+				one(card).checkPin("4321");
+				will(returnValue(false));
+			}}
+		);
+		assertTrue(cardManager.verify("1234"));
+		assertFalse(cardManager.verify("4321"));
 	}
 	
 	private CardManager cardManager;
