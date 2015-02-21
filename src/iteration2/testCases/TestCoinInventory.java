e05a8ba0b0858fe9064f1031eed9cb812ff5e480
Sean Cox <seanmdcox@gmail.com>
2015-02-21 16:46:49 -0700

    added file for testing coinInventory

---
+++ src/iteration2/testCases/TestCoinInventory.java
@@ -0,0 +1,14 @@
+package iteration2.testCases;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCoinInventory {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
